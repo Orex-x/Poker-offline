f69d9e79198429a9ccc4dcecec206ba3fb0e4f92
Orex-x <vipboy78038@gmail.com>
2023-11-09 14:07:46 +0300

    init client

---
+++ Client/Poker-offline/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Poker_offline;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Client/Poker-offline/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Poker_offline;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
